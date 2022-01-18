526b6537365fc81f9d9d2c0155b5295d7668d816
xiao_wine <26284241917@qq.com>
2022-01-18 17:11:04 +0800

    感谢名单解耦合

--- app/src/main/java/miui/statusbar/lyric/activity/AboutActivity.kt
+++ app/src/main/java/miui/statusbar/lyric/activity/AboutActivity.kt
@@ -20,31 +20,5 @@ class AboutActivity: PreferenceActivity() {
         addPreferencesFromResource(R.xml.about_preferences)
         actionBar?.setDisplayHomeAsUpEnabled(true)
         title = getString(R.string.About)
-
-
-        val thkList: Preference = findPreference("thkList")
-        thkList.onPreferenceClickListener = Preference.OnPreferenceClickListener {
-            AlertDialog.Builder(this).apply {
-                setTitle(R.string.ThkList)
-                setItems(
-                    arrayOf("潇风残月", "柒猫Sebun_Neko", "Moriafly", "Yife Playte", "YuKongA", "QQ little ice", "咕灵谷灵咕")
-                ) { _, which ->
-                    lateinit var url: String
-                    when (which) {
-                        0 -> url = "https://www.coolapk.com/u/666190"
-                        1 -> url = "https://www.coolapk.com/u/2129443"
-                        2 -> url = "https://github.com/Moriafly"
-                        3 -> url = "https://github.com/YifePlayte"
-                        4 -> url = "https://github.com/YuKongA"
-                        5 -> url = "https://github.com/qqlittleice"
-                        6 -> url = "https://www.coolapk.com/u/1854895"
-                    }
-                    startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
-                }
-                setNegativeButton(R.string.Done, null)
-                show()
-            }
-            true
-        }
     }
 }
