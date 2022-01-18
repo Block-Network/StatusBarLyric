08058e1bc5c8a1bc8637307a9378e9e1de127eef
方块君 <fkj@fkj233.cn>
2022-01-18 15:58:33 +0800

    增加网易云Hook日志

--- app/src/main/java/miui/statusbar/lyric/hook/app/Netease.kt
+++ app/src/main/java/miui/statusbar/lyric/hook/app/Netease.kt
@@ -2,12 +2,21 @@
 
 package miui.statusbar.lyric.hook.app
 
+import android.annotation.SuppressLint
+import android.app.ActivityManager
 import android.app.AndroidAppHelper
 import android.app.Notification
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.graphics.Bitmap
+import android.os.Bundle
+import android.os.Process
 import android.text.TextUtils
+import android.util.TypedValue
+import android.view.Gravity
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
 import com.microsoft.appcenter.AppCenter
 import com.microsoft.appcenter.analytics.Analytics
 import com.microsoft.appcenter.crashes.Crashes
@@ -27,6 +36,9 @@ import java.lang.reflect.Parameter
 class Netease(private val lpparam: LoadPackageParam) {
     var context: Context? = null
     var musicName = ""
+    lateinit var subView: TextView
+    var appLog = " (点击刷新)"
+    var otherLog = " (模糊Hook未启用)"
 
     private fun disableTinker(lpparam: LoadPackageParam) {
         val tinkerApp = "com.tencent.tinker.loader.app.TinkerApplication".findClassOrNull(lpparam.classLoader)
@@ -62,6 +74,7 @@ class Netease(private val lpparam: LoadPackageParam) {
                                 }
                             }
                         }
+                        otherLog = " (模糊Hook, 已找到[${unhookMap.size}])"
                     }
                 }
             })
@@ -86,16 +99,32 @@ class Netease(private val lpparam: LoadPackageParam) {
                         }
                     }
                 }
+                otherLog = " (模糊Hook, 已找到[${unhookMap.size}])"
             }
         }
     }
 
+    private fun getCurrentProcessName(context: Context): String {
+        val pid = Process.myPid()
+        val mActivityManager = context.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        for (appProcess in mActivityManager.runningAppProcesses) {
+            if (appProcess.pid == pid) {
+                return appProcess.processName
+            }
+        }
+        return ""
+    }
+
+    @SuppressLint("SetTextI18n")
     fun hook(){
         try {
             disableTinker(lpparam)
             "com.netease.cloudmusic.NeteaseMusicApplication".hookAfterMethod("attachBaseContext", Context::class.java, classLoader = lpparam.classLoader) {
                 try {
                     context = it.thisObject as Context
+                    if (context?.let { it1 -> getCurrentProcessName(it1) } !in arrayOf("com.netease.cloudmusic", "com.netease.cloudmusic:play")) return@hookAfterMethod
+                    LogUtils.e("网易云Hook Process: " + context?.let { it1 -> getCurrentProcessName(it1) })
+                    context?.let { it1 -> SettingHook(it1) }
                     val verCode: Int? = context?.packageManager?.getPackageInfo(lpparam.packageName, 0)?.versionCode
                     val verName: String? = context?.packageManager?.getPackageInfo(lpparam.packageName, 0)?.versionName
                     if (verCode!! > 8000041) {
@@ -116,6 +145,7 @@ class Netease(private val lpparam: LoadPackageParam) {
                         for (hookNotification: String in hookNotificationArr) {
                             try {
                                 XposedHelpers.findAndHookMethod(hookNotification.split("#")[0], lpparam.classLoader, hookNotification.split("#")[1], Notification::class.java, Boolean::class.javaPrimitiveType, HookMethod())
+                                appLog = " (状态栏歌词模式)"
                                 return@hookAfterMethod
                             } catch (e: XposedHelpers.ClassNotFoundError) {
                                 errorMsg = e.message.toString()
@@ -125,14 +155,14 @@ class Netease(private val lpparam: LoadPackageParam) {
                         for (hookString: String in hookStringArr) {
                             try {
                                 XposedHelpers.findAndHookMethod(hookString.split("#")[0], lpparam.classLoader, hookString.split("#")[1], String::class.java, HookMethod())
+                                appLog = " (状态栏歌词模式)"
                                 return@hookAfterMethod
                             } catch (e: XposedHelpers.ClassNotFoundError) {
                                 errorMsg = e.message.toString()
                             }
                         }
-
-                        LogUtils.e("墨 • 状态栏歌词\n不一定支持的网易云版本! $verName\n$errorMsg")
-                        LogUtils.toast(context, "不一定支持的网易云版本! $verName\n$errorMsg")
+                        appLog = " (不一定支持的网易云版本! $verName\n$errorMsg)"
+                        LogUtils.e("不一定支持的网易云版本! $verName\n$errorMsg")
                     } else {
                         val enableBTLyricClass: String
                         val enableBTLyricMethod: String
@@ -185,6 +215,7 @@ class Netease(private val lpparam: LoadPackageParam) {
                                 XposedHelpers.findAndHookMethod(getMusicLyricClass, lpparam.classLoader, getMusicLyricMethod, String::class.java, String::class.java, String::class.java, Long::class.javaPrimitiveType, Bitmap::class.java, String::class.java, getMusicLyricHook)
                             }
                             XposedHelpers.findAndHookMethod(enableBTLyricClass, lpparam.classLoader, enableBTLyricMethod, XC_MethodReplacement.returnConstant(true))
+                            appLog = " (蓝牙歌词模式)"
                         } catch (e: NoSuchMethodError) {
                             LogUtils.e("网易云Hook失败: $e")
                             LogUtils.e("正在尝试通用Hook")
@@ -194,13 +225,14 @@ class Netease(private val lpparam: LoadPackageParam) {
                                         if (it1.args[1] != null) {
                                             Utils.sendLyric(context, it1.args[1].toString(), "Netease")
                                             LogUtils.e("网易云通用： " + it1.args[1].toString())
+                                            appLog = " (通用方法)"
                                         }
                                     }
                                 }
                             } catch (mE: NoSuchMethodError) {
                                 LogUtils.e("网易云通用Hook失败: $mE")
                                 LogUtils.e("未知版本: $verCode")
-                                LogUtils.toast(context, "墨 • 状态栏歌词\n 未知版本: $verCode")
+                                appLog = " (未知版本: $verCode)"
                             }
                         }
                     }
@@ -210,6 +242,84 @@ class Netease(private val lpparam: LoadPackageParam) {
             }
         } catch (e: Throwable) {
             LogUtils.e("网易云状态栏歌词错误： " + e.message)
+        }
+    }
+
+    inner class SettingHook(context: Context) {
+        private var switchViewName = ""
+        private lateinit var titleView: TextView
+
+
+        init {
+            val settingActivityClass = "com.netease.cloudmusic.activity.SettingActivity".findClassOrNull(context.classLoader)
+            val allFields = settingActivityClass?.declaredFields
+            for (field in allFields!!) {
+                if (field.type.name.contains("Switch")) {
+                    switchViewName = field.name
+                    break
+                }
+            }
+
+            settingActivityClass.hookAfterMethod("onCreate", Bundle::class.java) {
+                val c = it.thisObject as Context
+                initView(c)
+            }
+        }
+
+        @SuppressLint("SetTextI18n")
+        fun initView(context: Context) {
+            var originalText: TextView? = null
+            //获取开关控件
+            val switchCompat: View = XposedHelpers.getObjectField(context, switchViewName) as View
+            //获取开关控件爸爸
+            val parent = switchCompat.parent as ViewGroup
+            //获取开关控件爷爷
+            val grandparent = parent.parent as ViewGroup
+
+            val linearLayout = LinearLayout(context)
+            val layoutParams = parent.layoutParams
+            linearLayout.layoutParams = layoutParams
+            linearLayout.background = parent.background
+            linearLayout.gravity = Gravity.CENTER_VERTICAL
+            linearLayout.orientation = LinearLayout.HORIZONTAL
+            grandparent.addView(linearLayout, 0)
+
+            titleView = TextView(context)
+            linearLayout.addView(titleView)
+            subView = TextView(context)
+            linearLayout.addView(subView)
+            titleView.text = "状态栏歌词"
+            subView.text = appLog + otherLog
+            start@ for (i in 0 until parent.childCount) {
+                if (parent.getChildAt(i) is TextView) {
+                    originalText = parent.getChildAt(i) as TextView
+                    break
+                } else if (parent.getChildAt(i) is ViewGroup) {
+                    for (j in 0 until (parent.getChildAt(i) as ViewGroup).childCount) {
+                        if ((parent.getChildAt(i) as ViewGroup).getChildAt(j) is TextView) {
+                            originalText = (parent.getChildAt(i) as ViewGroup).getChildAt(j) as TextView
+                            break@start
+                        }
+                    }
+                }
+            }
+
+            if (originalText != null) {
+                titleView.setTextColor(originalText.textColors)
+                titleView.setTextSize(TypedValue.COMPLEX_UNIT_PX, originalText.textSize)
+                titleView.setPadding(
+                    if (originalText.paddingLeft == 0) Utils.dp2px(
+                        context,
+                        10F
+                    ) else originalText.paddingLeft, 0, 0, 0
+                )
+                subView.setTextColor(originalText.textColors)
+                subView.setTextSize(TypedValue.COMPLEX_UNIT_PX, (originalText.textSize / 3.0 * 2.0).toInt().toFloat())
+            }
+
+            linearLayout.setOnClickListener {
+                subView.text = appLog + otherLog
+            }
         }
     }
 
--- app/src/main/java/miui/statusbar/lyric/utils/Utils.kt
+++ app/src/main/java/miui/statusbar/lyric/utils/Utils.kt
@@ -262,4 +262,13 @@ object Utils {
             }
         )
     }
+
+    /**
+     * dp2px
+     */
+    @JvmStatic
+    fun dp2px(context: Context, dpValue: Float): Int {
+        val scale = context.resources.displayMetrics.density
+        return (dpValue * scale + 0.5f).toInt()
+    }
 }
