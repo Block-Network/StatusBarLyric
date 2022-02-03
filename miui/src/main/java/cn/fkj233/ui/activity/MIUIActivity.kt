b0d66f3b9cf4fc121ac36d569f4f630c2a30c0c0
方块君 <fkj@fkj233.cn>
2022-02-03 16:32:25 +0800

    优化代码

--- miui/src/main/java/cn/fkj233/ui/activity/MIUIActivity.kt
+++ miui/src/main/java/cn/fkj233/ui/activity/MIUIActivity.kt
@@ -5,11 +5,13 @@ package cn.fkj233.ui.activity
 import android.app.Activity
 import android.content.SharedPreferences
 import android.os.Bundle
-import android.util.Log
 import android.view.Gravity
 import android.view.View
 import android.view.ViewGroup
-import android.widget.*
+import android.widget.FrameLayout
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import cn.fkj233.miui.R
 import cn.fkj233.ui.activity.data.DataBinding
 import cn.fkj233.ui.activity.fragment.MIUIFragment
