cba713c77b0c0caa686a34d5d6e8f18c7228261a
方块君 <86393520+577fkj@users.noreply.github.com>
2022-03-05 11:16:47 +0800

    更换appcenter key

--- app/src/main/java/statusbar/lyric/utils/Utils.kt
+++ app/src/main/java/statusbar/lyric/utils/Utils.kt
@@ -48,7 +48,7 @@ import java.util.*
 
 @SuppressLint("StaticFieldLeak")
 object Utils {
-    val appCenterKey = "ec91e89c-17d8-408b-a295-c6bf845b9f53"
+    val appCenterKey = "aa0a0b32-8d8d-43ca-8956-947e1ed8294d"
 
     private val hasMiuiSetting: Boolean = isPresent("android.provider.MiuiSettings")
     private val packNameToIconName = HashMap<String, String>().apply {
