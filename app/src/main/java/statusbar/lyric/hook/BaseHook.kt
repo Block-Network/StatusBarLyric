140f029c3fe08d26a66d1e97421bddc67add0ee1
xiao_wine <143322369ye>
2022-03-06 21:58:10 +0800

    修复最新ci无法使用

--- app/src/main/java/statusbar/lyric/hook/BaseHook.kt
+++ app/src/main/java/statusbar/lyric/hook/BaseHook.kt
@@ -1,14 +1,14 @@
 package statusbar.lyric.hook
 
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam
-import statusbar.lyric.utils.ActivityOwnSP
 import statusbar.lyric.utils.AppCenterUtils
 import statusbar.lyric.utils.Utils
+import statusbar.lyric.utils.XposedOwnSP
 
 abstract class BaseHook(lpparam: LoadPackageParam) {
     init {
-        if (ActivityOwnSP.ownSPConfig.getAppCenter()){
-            AppCenterUtils(Utils.appCenterKey, lpparam)
+        if (XposedOwnSP.config.getAppCenter()){
+        AppCenterUtils(Utils.appCenterKey, lpparam)
         }
     }
 
