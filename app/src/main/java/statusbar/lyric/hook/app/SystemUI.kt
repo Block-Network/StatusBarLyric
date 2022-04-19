8320885e1205ea17120c6fc99aa7c696dc1fb189
方块君 <fkj@fkj233.cn>
2022-04-19 16:21:06 +0800

    修复野人官改无法使用

--- app/src/main/java/statusbar/lyric/hook/app/SystemUI.kt
+++ app/src/main/java/statusbar/lyric/hook/app/SystemUI.kt
@@ -71,7 +71,7 @@ class SystemUI : BaseHook() {
     var musicServer: ArrayList<String> = arrayListOf("com.kugou", "com.netease.cloudmusic", "com.tencent.qqmusic.service", "cn.kuwo", "remix.myplayer", "cmccwm.mobilemusic", "com.meizu.media.music", "com.tencent.qqmusicplayerprocess.service.QQPlayerServiceNew")
 
     // base data
-    lateinit var application: Application
+    val application: Application by lazy { AndroidAppHelper.currentApplication() }
     lateinit var clock: TextView
     private lateinit var customizeView: TextView
     lateinit var lyricSwitchView: LyricSwitchView
@@ -252,7 +252,6 @@ class SystemUI : BaseHook() {
 
     private fun lyricInit(clock: TextView?) {
         LogUtils.e(LogMultiLang.sendLog)
-        application = AndroidAppHelper.currentApplication() // Get Application
 
         application.sendBroadcast(Intent().apply {
             action = "App_Server"
