0c0abcf0439ade91a16a62a9788755b247f42738
xiaowine <xiaowine0gmail.com>
2022-06-04 22:23:29 +0800

    优化默认配置

--- app/src/main/java/statusbar/lyric/config/Config.kt
+++ app/src/main/java/statusbar/lyric/config/Config.kt
@@ -205,7 +205,7 @@ class Config {
     }
 
     fun getCustomizeText(): String {
-        return config.optString("CustomizeText", "|")
+        return config.optString("CustomizeText", "")
     }
 
     fun setCustomizeText(str: String) {
