58ac1a444cc8ec3fd4a7cafb03f15f9ef73c1121
Howard Wu <40033067+Howard20181@users.noreply.github.com>
2022-09-27 14:51:18 +0800

    Add tips for `OnlyGetLyric`

--- app/src/main/java/statusbar/lyric/activity/SettingsActivity.kt
+++ app/src/main/java/statusbar/lyric/activity/SettingsActivity.kt
@@ -732,7 +732,7 @@ class SettingsActivity : MIUIActivity() {
             }
 
             register("advancedSettings", getString(R.string.AdvancedSettings), true) {
-                TextWithSwitch(TextV(textId = R.string.OnlyGetLyric), SwitchV("OnlyGetLyric"))
+                TextWithSwitch(TextV(textId = R.string.OnlyGetLyric, tipsId = R.string.OnlyGetLyricTips), SwitchV("OnlyGetLyric"))
                 TextWithSwitch(TextV(textId = R.string.TimeOff), SwitchV("TimeOff"))
                 TextSummaryArrow(TextSummaryV(textId = R.string.TimeOffTime, onClickListener = {
                     MIUIDialog(activity) {
