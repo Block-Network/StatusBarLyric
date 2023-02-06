5c70f2e9908e8bf30ff2f6cec51aa68331d9fa96
xiao_wine <xiaowine0@gmail.com>
2023-02-06 22:15:45 +0800

    简化代码

--- app/src/main/java/statusbar/lyric/hook/app/Meizu.kt
+++ app/src/main/java/statusbar/lyric/hook/app/Meizu.kt
@@ -1,19 +1,14 @@
 package statusbar.lyric.hook.app
 
-import android.content.Context
 import statusbar.lyric.hook.BaseHook
 import statusbar.lyric.hook.MeiZuStatusBarLyric
 import statusbar.lyric.utils.LogUtils
-import statusbar.lyric.utils.Utils
 import statusbar.lyric.utils.ktx.findClass
-import statusbar.lyric.utils.ktx.hookAfterConstructor
-import statusbar.lyric.utils.ktx.hookBeforeMethod
 import statusbar.lyric.utils.ktx.isNull
-import statusbar.lyric.utils.ktx.setReturnConstant
 
 class Meizu : BaseHook() {
     private val songInfo = "com.tencent.qqmusic.core.song.SongInfo".findClass()
-    lateinit var context: Context
+
 
     override fun hook() {
         super.hook()
@@ -21,16 +16,7 @@ class Meizu : BaseHook() {
         if (songInfo.isNull()) {
             MeiZuStatusBarLyric.guiseFlyme(true)
         } else {
-            "com.tencent.qqmusiccommon.util.music.RemoteLyricController".setReturnConstant("BluetoothA2DPConnected", result = true)
-            "com.tencent.qqmusiccommon.util.music.RemoteControlManager".hookAfterConstructor(Context::class.java) {
-                context = it.args[0] as Context
-            }
-            "com.tencent.qqmusiccommon.util.music.RemoteControlManager".hookBeforeMethod("updataMetaData", songInfo, String::class.java) {
-                val lyric = if (it.args[1].isNull()) return@hookBeforeMethod else it.args[1].toString()
-                it.args[1] = null // 去除妙播显示歌词
-                LogUtils.e("小米音乐: $lyric")
-                Utils.sendLyric(context, lyric, "Meizu")
-            }
+            QQLite().hook("Meizu")
         }
     }
 }
---
+++ app/src/main/java/statusbar/lyric/hook/app/QQLite.kt
@@ -0,0 +1,27 @@
+package statusbar.lyric.hook.app
+
+import android.content.Context
+import statusbar.lyric.utils.LogUtils
+import statusbar.lyric.utils.Utils
+import statusbar.lyric.utils.ktx.findClass
+import statusbar.lyric.utils.ktx.hookAfterConstructor
+import statusbar.lyric.utils.ktx.hookBeforeMethod
+import statusbar.lyric.utils.ktx.isNull
+import statusbar.lyric.utils.ktx.setReturnConstant
+
+class QQLite {
+    private val songInfo = "com.tencent.qqmusic.core.song.SongInfo".findClass()
+    fun hook(name:String) {
+        var context: Context? = null
+        "com.tencent.qqmusiccommon.util.music.RemoteLyricController".setReturnConstant("BluetoothA2DPConnected", result = true)
+        "com.tencent.qqmusiccommon.util.music.RemoteControlManager".hookAfterConstructor(Context::class.java) {
+            context = it.args[0] as Context
+        }
+        "com.tencent.qqmusiccommon.util.music.RemoteControlManager".hookBeforeMethod("updataMetaData", songInfo, String::class.java) {
+            val lyric = if (it.args[1].isNull()) return@hookBeforeMethod else it.args[1].toString()
+            it.args[1] = null // 去除妙播显示歌词
+            LogUtils.e("$name: $lyric")
+            Utils.sendLyric(context!!, lyric, name)
+        }
+    }
+}
