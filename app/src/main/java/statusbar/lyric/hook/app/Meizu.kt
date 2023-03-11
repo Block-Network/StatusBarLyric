216e4d561bc3192acbd4ae5fa38d7af93edc4399
xiao_wine <xiaowine0@gmailc.om>
2023-03-11 21:03:32 +0800

    修复QQLite判断错误

--- app/src/main/java/statusbar/lyric/hook/app/Meizu.kt
+++ app/src/main/java/statusbar/lyric/hook/app/Meizu.kt
@@ -2,21 +2,14 @@ package statusbar.lyric.hook.app
 
 import statusbar.lyric.hook.BaseHook
 import statusbar.lyric.hook.MeiZuStatusBarLyric
-import statusbar.lyric.utils.LogUtils
-import statusbar.lyric.utils.ktx.findClass
-import statusbar.lyric.utils.ktx.isNull
 
 class Meizu : BaseHook() {
-    private val songInfo = "com.tencent.qqmusic.core.song.SongInfo".findClass()
-
 
     override fun hook() {
         super.hook()
-        LogUtils.e(songInfo.isNull())
-        if (songInfo.isNull()) {
+        val isQQLite = QQLite().hook("Meizu")
+        if (!isQQLite) {
             MeiZuStatusBarLyric.guiseFlyme(true)
-        } else {
-            QQLite().hook("Meizu")
         }
     }
 }
--- app/src/main/java/statusbar/lyric/hook/app/QQLite.kt
+++ app/src/main/java/statusbar/lyric/hook/app/QQLite.kt
@@ -1,6 +1,7 @@
 package statusbar.lyric.hook.app
 
 import android.content.Context
+import de.robv.android.xposed.XposedHelpers
 import statusbar.lyric.utils.LogUtils
 import statusbar.lyric.utils.Utils
 import statusbar.lyric.utils.ktx.findClass
@@ -10,19 +11,23 @@ import statusbar.lyric.utils.ktx.isNull
 import statusbar.lyric.utils.ktx.setReturnConstant
 
 class QQLite {
-    private val songInfo = "com.tencent.qqmusic.core.song.SongInfo".findClass()
-    fun hook(name: String) {
-        if (songInfo.isNull()) return
-        var context: Context? = null
-        "com.tencent.qqmusiccommon.util.music.RemoteLyricController".setReturnConstant("BluetoothA2DPConnected", result = true)
-        "com.tencent.qqmusiccommon.util.music.RemoteControlManager".hookAfterConstructor(Context::class.java) {
-            context = it.args[0] as Context
-        }
-        "com.tencent.qqmusiccommon.util.music.RemoteControlManager".hookBeforeMethod("updataMetaData", songInfo, String::class.java) {
-            val lyric = if (it.args[1].isNull()) return@hookBeforeMethod else it.args[1].toString()
-            it.args[1] = null // 去除妙播显示歌词
-            LogUtils.e("$name: $lyric")
-            Utils.sendLyric(context!!, lyric, name)
+    fun hook(name: String): Boolean {
+        return try {
+            val songInfo = "com.tencent.qqmusic.core.song.SongInfo".findClass()
+            var context: Context? = null
+            "com.tencent.qqmusiccommon.util.music.RemoteLyricController".setReturnConstant("BluetoothA2DPConnected", result = true)
+            "com.tencent.qqmusiccommon.util.music.RemoteControlManager".hookAfterConstructor(Context::class.java) {
+                context = it.args[0] as Context
+            }
+            "com.tencent.qqmusiccommon.util.music.RemoteControlManager".hookBeforeMethod("updataMetaData", songInfo, String::class.java) {
+                val lyric = if (it.args[1].isNull()) return@hookBeforeMethod else it.args[1].toString()
+                it.args[1] = null // 去除妙播显示歌词
+                LogUtils.e("$name: $lyric")
+                Utils.sendLyric(context!!, lyric, name)
+            }
+            true
+        } catch (_: XposedHelpers.ClassNotFoundError) {
+            false
         }
     }
 }
