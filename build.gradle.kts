3da2e17e10b417732d2f8ce7fffe92eff388e2ee
xiao_wine <xiaowine0@gmail.com>
2023-10-26 13:29:07 +0800

    改错了，同时我不理解为什么找不到库

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ buildscript {
     repositories {
         google()
         mavenCentral()
-        maven("https://jitpack.io")
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.1.2")
