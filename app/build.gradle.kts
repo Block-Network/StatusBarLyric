a51617ade426faefbb9d96ece183871832d022bb
xiao_wine <xiaowine0@gmail.com>
2023-11-04 19:49:29 +0800

    6.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "statusbar.lyric"
         minSdk = 26
         targetSdk = 34
-        versionCode = 202
-        versionName = "6.0.2"
+        versionCode = 203
+        versionName = "6.1.0"
         aaptOptions.cruncherEnabled = false
         aaptOptions.useNewCruncher = false
         buildConfigField("long", "BUILD_TIME", "$buildTime")
