290eeabf64434b7e4e48ace4e5b1eaa3ec1a45e6
xiao_wine <68009112+xiaowine@users.noreply.github.com>
2023-11-04 23:39:47 +0800

    6.1.1
    添加 MIUI for Pad优化
    添加 长按状态栏停止音乐 
    添加 歌词动画插值器、歌词动画持续时间和动画淡出淡入效果
    删除 正则表达式屏蔽歌词
    修复 滑动状态栏无法切歌 修复开启限制可见性变化后 点击隐藏歌词 时间不显示
    修复 miui切换深色后无法使用
    修复 部分界面异常
    优化 点击状态栏隐藏歌词
    优化 代码

--- app/src/main/java/statusbar/lyric/activity/page/MenuPage.kt
+++ app/src/main/java/statusbar/lyric/activity/page/MenuPage.kt
@@ -63,9 +63,9 @@ class MenuPage : BasePage() {
 
         Line()
         TitleText(textId = R.string.module_version)
-        TextSummaryV(textId = R.string.module_version, tips = "${BuildConfig.VERSION_NAME}(${BuildConfig.VERSION_CODE})-${BuildConfig.BUILD_TYPE}")
+        TextS(textId = R.string.module_version, tips = "${BuildConfig.VERSION_NAME}(${BuildConfig.VERSION_CODE})-${BuildConfig.BUILD_TYPE}")
         val buildTime = SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.getDefault()).format(BuildConfig.BUILD_TIME)
-        TextSummaryV(textId = R.string.module_build_time, tips = buildTime)
+        TextS(textId = R.string.module_build_time, tips = buildTime)
         Text()
     }
 }
