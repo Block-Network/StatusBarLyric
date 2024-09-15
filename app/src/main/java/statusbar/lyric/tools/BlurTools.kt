e291d6f41916df4a2fc84a4f1ca7e410c6ea9158
余空 <70465933+YuKongA@users.noreply.github.com>
2024-09-15 18:50:08 +0800

    Add a based background color

--- app/src/main/java/statusbar/lyric/tools/BlurTools.kt
+++ app/src/main/java/statusbar/lyric/tools/BlurTools.kt
@@ -1,11 +1,15 @@
 package statusbar.lyric.tools
 
+import android.graphics.Color
 import android.view.View
 
 object BlurTools {
+
     val blurRadio = 25
     val cornerRadius = cornerRadius(32f)
-    val blendModes = arrayOf(intArrayOf(0x8f616060.toInt(), 18), intArrayOf(0xa3f2f2f2.toInt(), 3))
+    val blendModes = arrayOf(
+        intArrayOf(106, Color.parseColor("#20818181"))
+    )
 
     private val setBackgroundBlur by lazy {
         View::class.java.getDeclaredMethod("setBackgroundBlur", Integer.TYPE, FloatArray::class.java, Array<IntArray>::class.java)
