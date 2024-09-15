727efb62f2b310ee66cfb2da024b3ebbb9c1f6ad
余空 <70465933+YuKongA@users.noreply.github.com>
2024-09-15 21:28:09 +0800

    Bump all deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.2.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.21")
+        classpath("com.android.tools.build:gradle:8.6.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.20")
     }
 }
 
