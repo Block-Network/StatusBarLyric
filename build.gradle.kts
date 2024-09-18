71da0765554c82987e79a5098115874a94d1c6ed
余空 <70465933+YuKongA@users.noreply.github.com>
2024-09-18 15:37:59 +0800

    Use WeakReference

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,11 +4,11 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.6.0")
+        classpath("com.android.tools.build:gradle:8.6.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.20")
     }
 }
 
 tasks.register("Delete", Delete::class) {
-    delete(rootProject.buildDir)
+    delete(layout.buildDirectory)
 }
